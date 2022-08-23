5786000815c932f86563d92fac63bf2747da9d13
Koroleva Maria <mkoroleva5@gmail.com>
2022-08-23 14:23:46 +0300

    init: add webpack configs

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import "./styles/index.css"
+import deckImage from "./assets/mythicCardBackground.png"
+
+import ancientsData from './data/ancients'
+
+const ancients = document.querySelectorAll('.ancient-card');
+const ancientsContainer = document.querySelector('.ancients-container');
+const stages = document.querySelectorAll('.stage');
+const deck = document.querySelector('.deck');
+
+ancientsContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target.classList.contains('ancient-card')) {
+        for(let i = 0; i < ancients.length; i++) {
+            ancients[i].classList.remove('active');
+        }
+        target.classList.add('active');
+    }
+    for(let i = 0; i < ancients.length; i++) {
+        if (ancients[i].classList.contains('active')) {
+            stages[0].textContent = ancientsData[i].firstStage.greenCards;
+            stages[1].textContent = ancientsData[i].firstStage.brownCards;
+            stages[2].textContent = ancientsData[i].firstStage.blueCards;
+            stages[3].textContent = ancientsData[i].secondStage.greenCards;
+            stages[4].textContent = ancientsData[i].secondStage.brownCards;
+            stages[5].textContent = ancientsData[i].secondStage.blueCards;
+            stages[6].textContent = ancientsData[i].thirdStage.greenCards;
+            stages[7].textContent = ancientsData[i].thirdStage.brownCards;
+            stages[8].textContent = ancientsData[i].thirdStage.blueCards;
+        }
+    }
+})
+
+const difficulty = document.querySelectorAll('.difficulty');
+const difficultyContainer = document.querySelector('.difficulty-container');
+
+difficultyContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target.classList.contains('difficulty')) {
+        for(let i = 0; i < difficulty.length; i++) {
+            difficulty[i].classList.remove('active');
+        }
+        target.classList.add('active');
+    }
+    if (ancients[0].contains.classList('active')) {
+        deck.style.backgroundImage = `url('${deckImage}')`
+    }
+})
+
+
+
+
